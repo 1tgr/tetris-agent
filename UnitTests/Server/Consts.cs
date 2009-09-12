abdc26670aef714b9ff401a3c5d50f586497ad07
timrobinson <tim.g.robinson@gmail.com>
2009-09-12 14:14:38 +0100

    Add missing files

---
+++ UnitTests/Server/Consts.cs
@@ -0,0 +1,8 @@
+namespace Tim.Tetris.UnitTests.Server
+{
+    public static class Consts
+    {
+        public const string Board = ".......... .......... .......... .......... .......... .......... .......... .......... .......... .......... .......... .......... .......... .......... .......... .......... .......... .......... .......... zzzzzzzz..";
+        public const string Piece = "l";
+    }
+}
---
+++ UnitTests/Server/TetrisAgentTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using Rhino.Mocks;
+using Tim.Tetris.Server;
+
+namespace Tim.Tetris.UnitTests.Server
+{
+    [TestFixture]
+    public class TetrisAgentTests
+    {
+        [Test]
+        public void ShouldGetNextMove()
+        {
+            Random random = MockRepository.GenerateStrictMock<Random>();
+            TetrisAgent agent = new TetrisAgent(random);
+            random.Stub(r => r.Next(3)).Return(0);
+            Assert.AreEqual(new TetrisMove(8, 0), agent.GetNextMove(Consts.Piece, Consts.Board));
+        }
+    }
+}
